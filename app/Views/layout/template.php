dfad5dad5e32c2da722ceb871cbbcc78971fb939
kuhakuni <afif.haidar@gmail.com>
2022-01-21 16:52:02 +0700

    Add Active class script

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -21,7 +21,15 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous">
     </script>
-
+    <script>
+    let navLink = document.querySelectorAll('.nav-item .nav-link');
+    navLink.forEach(function(link) {
+        if (link.href == window.location.href) {
+            link.classList.add('active');
+            link.setAttribute('aria-current', 'page');
+        }
+    });
+    </script>
 </body>
 
 </html>
