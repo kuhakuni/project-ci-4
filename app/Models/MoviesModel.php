04552fa4dcbbfc3198a13174674db005ba1fab19
kuhakuni <afif.haidar@gmail.com>
2022-01-22 08:11:23 +0700

    Add find by slug method

--- app/Models/MoviesModel.php
+++ app/Models/MoviesModel.php
@@ -11,4 +11,12 @@ class MoviesModel extends Model
 	protected $useTimeStamps = true;
 	protected $createdField = "CREATED_AT";
 	protected $updatedField = "UPDATED_AT";
+
+	public function findBySlug($slug)
+	{
+		if (!$slug) {
+			return $this->findAll();
+		}
+		return $this->where(["SLUG" => $slug])->first();
+	}
 }
